bef48d86314d3bdcc41d65d0dd28ae56e50a6935
TamariTs <takotsikara@gmail.com>
2025-03-28 15:32:25 +0400

    save changes

--- javahomework.js
+++ javahomework.js
@@ -13,4 +13,27 @@
 //marks:10,
 //}
 //console.log(student.firstName)
-jhjhjh
+//3.გამოაცხადეთ ორი ცვლადი m და n მნიშვნელობებით 45 და 23, 
+// დაბეჭდეთ მათი ჯამი თუ m მეტია n ზე , თუ არადა დაბეჭდეთ n.
+//let m=45, n=23
+//if (m>n) {
+   // console.log (m+n)
+//} else {console.log (n)}
+//4.გამოაცხადეთ ცვლადი studentName და მიანიჭეთ 
+// მნიშვნელობად თქვენი სახელი. დაბეჭდეთ “Rise and shine, “თქვენი სახელი” !” 
+// ( მაგ თუ სახელი არის Irakli  უნდა დაიბეჭდოს Rise and shine, Irakli ! )
+//let student=
+//{studentName:"Tamari",
+//NickName:"Rise and shine, Tamari",
+//}
+//console.log (student.NickName)
+
+//6.გამოაცხადეთ ცვლადი a, b და c. სადაც a = 23, b=a-ნახევარი, c= a-ს და b-ს ჯამი. 
+// დაბეჭდეთ c.
+//let a=23, b=a/2, c=a+b
+//{console.log (c)}
+
+//7.გამოაცხადეთ მასივი phones ელემენტებით : Iphone, Samsung, Pixel, Huawei, Xiaomi და დაბეჭდეთ.
+
+//let phones=["Iphone", "Samsung", "Pixel", "Huawei", "Xiaomi"]
+//{console.log (phones[0],phones[1],phones[2],phones[3],phones[4])}
