2e122cf76ed7a58dbd5a6a7d6523a1b014c15b2a
TamariTs <takotsikara@gmail.com>
2025-03-28 12:55:10 +0400

    update

---
+++ javahomework.js
@@ -0,0 +1,15 @@
+//1. let a=87, b=65
+//{
+   // console.log(a+b)
+//}
+
+//2.გამოაცხადეთ ობიექტი student, ფროფერთებით: firstName, 
+// lastName, age, email, marks, დაბეჭდეთ student ის სახელი
+
+//let student={   
+//firstName:"Tamari",
+//lastName:"Tsik",
+//age:34,
+//marks:10,
+//}
+//console.log(student.firstName)
